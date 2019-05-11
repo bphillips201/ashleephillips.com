bb48450b3c50d121ac8b655a261efdc8cdeb6edd
Brian Phillips <brianphillips@phillips-macbook.local>
2019-05-10 21:07:21 -0700

    added basic files

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,39 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // your wordpress source
+        baseUrl: `ashleephillips.com`,
+        protocol: `https`,
+        // is it hosted on wordpress.com, or self-hosted?
+        hostingWPCOM: false,
+        // does your site use the Advanced Custom Fields Plugin?
+        useACF: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Merriweather`,
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`, `400i`, `700i`],
+          },
+          {
+            family: `Poppins`,
+            variants: [`700`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,13 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Home" keywords={[`ux writer`, `accessibility`, `speaker`]} />
+    <h1>Hi, my dudes</h1>
+    <p>
+      I’m a User Experience Writer living on the California central coast. I
+      speak about web accessbility and geek out over calligraphy and Bob’s
+      Burgers.
+    </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
