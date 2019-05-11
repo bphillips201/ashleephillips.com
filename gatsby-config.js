93ff02afedf9848e017b19ef1dca54fe1dbd2f9c
Brian Phillips <brianphillips@phillips-macbook.local>
2019-05-11 00:12:44 -0700

    added nprogress loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,15 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `#911b35`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
